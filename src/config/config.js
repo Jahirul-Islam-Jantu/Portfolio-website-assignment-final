b770b574d80bb9e02376c1e0791b4a5f7fc6f2ad
Jahirul Islam <jahirulislamjantu002@gmail.com>
2025-02-06 08:51:35 +0600

    config file

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,9 @@
+import dotenv from "dotenv";
+dotenv.config();
 
-     export const MONGODB_CONNECTION = "mongodb+srv://unchangedjahir:jan742682@mernstack.tose2.mongodb.net/Full-Stack" || "mongodb://localhost:27017/full-stack-project"
- export const JWT_SECRET = "05660b74f5658917c96d0068d9e62455a0abca27a10438585aaec933e902a2d2"
+
+export const MONGODB_CONNECTION = process.env.MONGODB_CONNECTION;
+export const JWT_SECRET = process.env.JWT_SECRET;
 export const JWT_EXPIRATION_DURATION = 60 * 60 * 24 * 7 * 1000;
 
 export const Cookie_ExpiresIn = 60 * 60 * 24 * 7 * 1000;
@@ -9,10 +12,6 @@ export const corsOptions = {
         credentials: true,
     };
 
-// export const EMAIL_HOST = "smtp.gmail.com"
-// export const EMAIL_PORT = 465
-// export const EMAIL_USER = "jahirulislamjantu@gmail.com"
-// export const EMAIL_PASSWORD = "gefnoodpvwmzwfgg"
 
 export const URL_ENCODING = true;
 export const MAX_JSON_SIZE = "50mb";
